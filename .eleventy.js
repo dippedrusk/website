c6c08ccc5c69673efc28b587c5c17483f5492d12
vgautam <vasundhara1997@gmail.com>
2021-09-18 01:18:10 -0700

    add js functions and filters

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const markdownIt = require("markdown-it");
+const { DateTime } = require("luxon");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("static");
@@ -9,6 +10,8 @@ module.exports = function (eleventyConfig) {
     breaks: false,
     linkify: true,
   });
+
+  eleventyConfig.setDataDeepMerge(true);
 
   eleventyConfig.setLibrary("md", markdownLibrary);
 
@@ -29,6 +32,42 @@ module.exports = function (eleventyConfig) {
     ui: false,
     ghostMode: false,
   });
+
+  eleventyConfig.addFilter("readableDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat(
+      "dd LLL yyyy"
+    );
+  });
+
+  // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+  eleventyConfig.addFilter("htmlDateString", (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("yyyy-LL-dd");
+  });
+
+  // Get the first `n` elements of a collection.
+  eleventyConfig.addFilter("head", (array, n) => {
+    if (!Array.isArray(array) || array.length === 0) {
+      return [];
+    }
+    if (n < 0) {
+      return array.slice(n);
+    }
+
+    return array.slice(0, n);
+  });
+
+  // Return the smallest number argument
+  eleventyConfig.addFilter("min", (...numbers) => {
+    return Math.min.apply(null, numbers);
+  });
+
+  function filterTagList(tags) {
+    return (tags || []).filter(
+      (tag) => ["all", "nav", "post", "posts"].indexOf(tag) === -1
+    );
+  }
+
+  eleventyConfig.addFilter("filterTagList", filterTagList);
 
   return {
     dir: {
