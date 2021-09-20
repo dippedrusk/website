7bd9d538ebc84809ff08863b34fbe649eb13145c
vgautam <vasundhara1997@gmail.com>
2021-09-20 03:31:49 -0700

    add support for syntax highlighting

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,11 @@
 const fs = require("fs");
 const markdownIt = require("markdown-it");
 const { DateTime } = require("luxon");
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(syntaxHighlight);
+
   eleventyConfig.addPassthroughCopy("static");
 
   let markdownLibrary = markdownIt({
